a23a04d82ed4ead85a8eea181cc07f423b6d81ed
Your Name <kait.klochkova@gmail.com>
2012-03-14 23:25:57 +0400

    changed StackArray

--- 2semester/Homework4/task2/src/task2/StackArray.java
+++ 2semester/Homework4/task2/src/task2/StackArray.java
@@ -8,19 +8,29 @@ package task2;
  *
  * @author Miracle
  */
-class StackArray{
+class StackArray {
+
     StackArray() {
         int[] array = new int[1000];
+        count = 0;
     }
-    public void push(int valueForAdd) {
-        array[array.length] = valueForAdd;
+
+    public boolean push(int valueForAdd) {
+        if (count < 1000) {
+            array[count] = valueForAdd;
+            return true;
+        } else {
+            return false;
+        }
     }
+
     public int top() {
         return array[array.length];
     }
+
     public void pop() {
-            array[array.length] = 0;
+        array[array.length] = 0;
     }
     private int[] array;
+    private int count;
 }
-
