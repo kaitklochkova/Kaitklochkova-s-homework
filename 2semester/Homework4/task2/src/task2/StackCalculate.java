57d2489fa00c0da8dccd94cd7a37bd8cbd419aac
Your Name <kait.klochkova@gmail.com>
2012-04-04 12:44:30 +0400

    add the Java-DOC comments to StackCalculate

--- 2semester/Homework4/task2/src/task2/StackCalculate.java
+++ 2semester/Homework4/task2/src/task2/StackCalculate.java
@@ -8,44 +8,73 @@ package task2;
  *
  * @author Miracle
  */
-interface Stack {
-
+interface StackInterface {
+    
+    /**
+     * push value to the stack
+     *
+     * @param value value, which you want to push
+     */
     void push(int val);
 
+    /**
+     * pop an element from the stack
+     */
     int pop();
 
+    /**
+     * to return value of the top element
+     *
+     * @return value of the top element
+     */
     int top();
-
+    
+    /**
+     * checks that the stack is empty or not
+     *
+     * @return true if stack is empty else false
+     */
     boolean isEmpty();
 }
 
 public class StackCalculate {
 
-    StackCalculate (Stack stack) {
-        
+    /**
+     * constructor for calculator
+     *
+     * @param stack
+     */
+    StackCalculate () {
+       this.stack = stack;
     }
-            
-    public int add(Stack stack) {
+    
+    /**
+     * adds two values
+     *
+     * @return summ two values
+     */
+    public int add() {
         return (stack.pop() + stack.pop());
     }
 
-    int sub(Stack stack) {
+    int sub() {
         return (stack.pop() - stack.pop());
     }
 
-    int mul(Stack stack) {
+    int mul() {
         return (stack.pop() * stack.pop());
     }
 
-    int div(Stack stack) {
+    int div() {
         return (stack.pop() / stack.pop());
     }
     
-    void printResult(Stack stack) {
+    void printResult(StackInterface stack) {
         System.out.format("%d\n", stack.pop());
     }
 
     /**
-     * @param args the command line arguments
+     * the stack of the calculator
      */
+    private StackInterface stack;
 }
