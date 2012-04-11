bca4e808516b3cb388bb041fb308218bc93287b0
Your Name <kait.klochkova@gmail.com>
2012-04-11 15:44:08 +0400

    add hw6.2 clock and hw7.2 set

---
+++ 2semester/Homework7/hw7.task2/Set/src/set/Set.java
@@ -0,0 +1,132 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package set;
+
+/**
+ *
+ * @author Miracle
+ */
+public class Set<Type> {
+
+    /**
+     * constructor for set
+     */
+    Set() {
+        head = null;
+    }
+
+    /*
+     * add value to the set
+     *
+     * @param valueForAdd value, which you want to add
+     */
+    public void add(Type valueForAdd) throws IsInSet {
+        if (isElementInSet(valueForAdd)) {
+            throw new IsInSet();
+        } else {
+            SetElement newEl = new SetElement();
+            newEl.next = head;
+            newEl.value = valueForAdd;
+            head = newEl;
+        }
+    }
+
+    /*
+     * del value from the set
+     *
+     * @param valueForDel value, which you want to del
+     */
+    public void delete(Type valueForDel) throws NotInSet {
+        if (!isElementInSet(valueForDel)) {
+            throw new NotInSet();
+        } else {
+            SetElement i = head;
+            while (i != null && i.value == valueForDel) {
+                head = i.next;
+                i = head;
+            }
+            while (i != null && i.next != null) {
+                if (i.next.value == valueForDel) {
+                    i.next = i.next.next;
+                } else {
+                    i = i.next;
+                }
+            }
+        }
+    }
+
+    /**
+     * checks that the value is in Set
+     *
+     * @return true if value is in set else false
+     */
+    public boolean isElementInSet(Type value) {
+        SetElement i = head;
+        while (i != null) {
+            if (i.value == value) {
+                return true;
+            } else {
+                i = i.next;
+            }
+        }
+        return false;
+    }
+
+    public Set sum(Set set) throws IsInSet {
+        Set newSet = new Set();
+        SetElement i = head;
+        newSet.add(i.value);
+        while (i.next != null) {
+            newSet.add(i.next.value);
+            i = i.next;
+        }
+        i = set.head;
+        newSet.add(i.value);
+        while (i.next != null) {
+            newSet.add(i.next.value);
+            i = i.next;
+        }
+        return newSet;
+    }
+
+    public Set cross(Set set) throws IsInSet {
+        Set newSet = new Set();
+        SetElement i1 = head;
+        SetElement i2 = set.head;
+        while (i1 != null) {
+            while (i2 != null) {
+                if (i1.value == i2.value) {
+                    newSet.add(i1.value);
+                }
+                i2 = i2.next;
+            }
+            i1 = i1.next;
+        }
+        return newSet;
+    }
+
+    private class SetElement {
+
+        /**
+         * value of setElement
+         */
+        Type value;
+        /**
+         * next of setElement
+         */
+        SetElement next;
+    }
+    /**
+     * head of set
+     */
+    private SetElement head;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ 2semester/Homework7/hw7.task2/Set/test/set/SetTest.java
@@ -0,0 +1,176 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package set;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Miracle
+ */
+public class SetTest {
+
+    public SetTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /*
+     * Test of add method, of class Set
+     */
+    @Test
+    public void testAdd() throws IsInSet {
+        Set<Integer> set = new Set<Integer>();
+        set.add(1);
+        assertTrue(set.isElementInSet(1));
+    }
+
+    /*
+     * Test of add method, of class Set
+     */
+    @Test
+    public void testAddSomeElements() throws IsInSet {
+        Set<Integer> set = new Set<Integer>();
+        set.add(1);
+        set.add(2);
+        set.add(4);
+        set.add(3);
+        assertTrue(set.isElementInSet(3));
+    }
+
+    /*
+     * Test of add method, of class Set
+     */
+    @Test
+    public void testAddTwoSameElements() throws IsInSet {
+        Set<Integer> set = new Set<Integer>();
+        set.add(1);
+        boolean throwed = false;
+        try {
+            set.add(1);
+        } catch (IsInSet isInSet) {
+            throwed = true;
+        }
+        assertTrue(throwed);
+    }
+
+    /*
+     * Test of del method, of class Set
+     */
+    @Test
+    public void testDel() throws NotInSet, IsInSet {
+        Set<Integer> set = new Set<Integer>();
+        set.add(2);
+        set.delete(2);
+        assertFalse(set.isElementInSet(2));
+    }
+
+    /*
+     * Test of del method, of class Set
+     */
+    @Test
+    public void testDelFirstElement() throws NotInSet, IsInSet {
+        Set<Integer> set = new Set<Integer>();
+        set.add(2);
+        set.add(3);
+        set.add(1);
+        set.delete(1);
+        assertFalse(set.isElementInSet(1));
+    }
+
+    /*
+     * Test of del method, of class Set
+     */
+    @Test
+    public void testDelLastElement() throws NotInSet, IsInSet {
+        Set<Integer> set = new Set<Integer>();
+        set.add(2);
+        set.add(3);
+        set.add(1);
+        set.delete(2);
+        assertFalse(set.isElementInSet(2));
+    }
+
+    /*
+     * Test of del method, of class Set
+     */
+    @Test
+    public void testDelMidleElement() throws NotInSet, IsInSet {
+        Set<Integer> set = new Set<Integer>();
+        set.add(2);
+        set.add(3);
+        set.add(1);
+        set.delete(3);
+        assertFalse(set.isElementInSet(3));
+    }
+
+    /*
+     * Test of del method, of class Set
+     */
+    @Test
+    public void testDelEmptySet() throws NotInSet {
+        Set<Integer> set = new Set<Integer>();
+        boolean throwed = false;
+        try {
+            set.delete(2);
+        } catch (NotInSet notInSet) {
+            throwed = true;
+        }
+        assertEquals(true, throwed);
+    }
+
+    /*
+     * Test of sum method, of class Set
+     */
+    @Test
+    public void testSum() throws NotInSet, IsInSet {
+        Set<Integer> set1 = new Set<Integer>();
+        Set<Integer> set2 = new Set<Integer>();
+        for (int i = 0; i < 2; i++) {
+            set1.add(i);
+        }
+        for (int i = 2; i < 4; i++) {
+            set2.add(i);
+        }
+        Set<Integer> set = set1.sum(set2);
+        for (int i = 0; i < 4; i++) {
+            assertTrue(set.isElementInSet(i));
+        }
+    }
+    /*
+     * Test of cross method, of class Set
+     */
+
+    @Test
+    public void testCross() throws NotInSet, IsInSet {
+        Set<Integer> set1 = new Set<Integer>();
+        Set<Integer> set2 = new Set<Integer>();
+        for (int i = 0; i < 4; i++) {
+            set1.add(i);
+        }
+        for (int i = 2; i < 6; i++) {
+            set2.add(i);
+        }
+        Set<Integer> set = set1.cross(set2);
+        //for (int i = 2; i < 6; i++) {
+            assertTrue(set.isElementInSet(6));
+        //}
+    }
+}
