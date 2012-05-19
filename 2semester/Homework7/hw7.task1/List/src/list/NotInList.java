08c8252d5b1fbd8110acf06fabd2901dd28446db
Your Name <kait.klochkova@gmail.com>
2012-05-19 19:45:50 +0400

    add NotInList.class exception

---
+++ 2semester/Homework7/hw7.task1/List/src/list/NotInList.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package list;
+
+/**
+ *
+ * @author Miracle
+ */
+public class NotInList extends Exception {
+    /**
+     * constructor for NotInList exception
+     */
+    public NotInList() {
+        super();
+    }
+
+}
