2d8a51e028b223ba07be285cf3c175029be88de5
Ekaterina <kait.klochkova@gmail.com>
2013-03-25 10:51:03 +0400

    first homework of 4 semester

---
+++ 4semester/Homework1/task1/task1/Program.fs
@@ -0,0 +1,14 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+
+let number = 6
+
+let rec fact x = 
+    match x with
+    | 0 -> 1
+    | _ -> fact (x - 1) * x
+
+let result = fact number
+
+System.Console.WriteLine(result)
+ignore(System.Console.Read())
+
---
+++ 4semester/Homework1/task2/task2/Program.fs
@@ -0,0 +1,14 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+
+let number = 5
+
+let rec fib n = 
+    match n with
+    | 0 -> 0
+    | 1 -> 1
+    | n -> n + fib(n - 1)
+
+let result = fib number
+
+System.Console.WriteLine(result)
+ignore(System.Console.Read())
