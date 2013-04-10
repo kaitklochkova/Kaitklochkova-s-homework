d27c90319f86558bd748483c1dce0c2962a6f833
Ekaterina <kait.klochkova@gmail.com>
2013-04-11 01:11:01 +0400

    added homework2 task5

---
+++ 4semester/Homework2/task5/task5/Program.fs
@@ -0,0 +1,9 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+
+let testPalindrom (myString : string) = 
+    let stringArray = myString.ToCharArray()
+    stringArray = Array.rev stringArray
+ 
+System.Console.WriteLine(testPalindrom "ololo")
+System.Console.WriteLine(testPalindrom "matmex")
+ignore(System.Console.Read())
