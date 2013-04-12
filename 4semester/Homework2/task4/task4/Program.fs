3cb38cb68b9cbb11b4352e8393c89c9d83254131
Ekaterina <kait.klochkova@gmail.cpm>
2013-04-12 15:12:07 +0400

    added homework2 task4

---
+++ 4semester/Homework2/task4/task4/Program.fs
@@ -0,0 +1,14 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+
+let rec first list comparingNumber position = 
+    match list with
+    |[] -> 0
+    |h::t ->
+        if (comparingNumber = list.Head)
+        then
+            position
+        else
+            first list.Tail comparingNumber (position + 1)
+
+System.Console.Write(first [0; 1; 2; 3; 4; 5; 6; 7; 8; 9] 5 1)
+ignore(System.Console.Read())
