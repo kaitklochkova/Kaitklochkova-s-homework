8b9817607ea8d4afc797c8205b1b4fec1e8c8943
Ekaterina <kait.klochkova@gmail.cpm>
2013-05-26 16:59:54 +0400

    added homework6

---
+++ 4semester/Homework6/task1/task1/Program.fs
@@ -0,0 +1,28 @@
+﻿// Дополнительные сведения о F# см. на http://fsharp.net
+
+type RoundedBuilder (accuracy : int) =
+
+    member this.Bind (x, rest : float -> float) =
+        System.Math.Round(rest x, accuracy)
+
+    member this.Return answer = answer
+
+let rounding accuracy = RoundedBuilder accuracy
+
+let test1 =
+    rounding 3 {
+        let! a = 2.0 / 12.0
+        let! b = 3.5
+        return a / b
+    }
+    
+let test2 =
+    rounding  4 {
+        let! a = 2.0 / 12.0
+        let! b = 3.5
+        return a / b
+    }
+
+System.Console.WriteLine(test1)
+System.Console.WriteLine(test2)
+ignore(System.Console.Read())
