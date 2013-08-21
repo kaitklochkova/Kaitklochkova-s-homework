901278cd0a33c212e75dadc99b68859f93aa589f
Ekaterina <kait.klochkova@gmail.cpm>
2013-08-22 01:37:00 +0400

    coursework

---
+++ 4semester/Coursework/server/server/Program.fs
@@ -0,0 +1,17 @@
+﻿open System.Net.Sockets
+open System.Net
+open System  
+
+let IP = Convert.ToInt64("127.0.0.1") 
+let server_IP = new IPAddress(IP)
+
+let handler =
+    printf("I have a request")
+
+let listener = 
+    let new_listener = new TcpListener(server_IP, 1)
+    while true do
+         if new_listener.Pending() 
+         then
+            handler
+         
