7333d11ad43876bcaa498b22ece68b3ea2ae4739
Ekaterina <kait.klochkova@gmail.cpm>
2013-09-20 00:38:09 +0400

    finish coursework

--- 4semester/Coursework/panel/Program.fs
+++ 4semester/Coursework/panel/Program.fs
@@ -1,47 +1,64 @@
 ﻿open System
 open System.Drawing
 open System.Windows.Forms
-open System.Net
-open Microsoft.SqlServer.Server
-open System.IO
 
 // Create form
-let form = new Form(Text = "Control panel of robots", Size = new Size(370, 350))
+let form = new Form(Text = "Control panel of robots", Size = new Size(400, 300))
+form.MinimumSize <- new System.Drawing.Size(300, 250)
 
-let tableLayoutPanel = new TableLayoutPanel(ColumnCount = 3,  RowCount = 4) 
-//For clearing the existing table
-tableLayoutPanel.Controls.Clear();
-//Clear out the existing row and column styles
-tableLayoutPanel.ColumnStyles.Clear();
-tableLayoutPanel.RowStyles.Clear();
-
-let columnStyles = new ColumnStyle(SizeType.AutoSize)
-
-ignore(tableLayoutPanel.ColumnStyles.Add(columnStyles))
-
-// Create button of control (up, down, left, right) and add button to form
-let buttonUp = new Button(Text = "UP", Size = new Size(60, 60))
-tableLayoutPanel.Controls.Add(buttonUp, 1, 0)
-let buttonDown = new Button(Text = "DOWN", Size = new Size(60, 60))
-tableLayoutPanel.Controls.Add(buttonDown, 1, 2)
-let buttonLeft = new Button(Text = "LEFT", Size = new Size(60, 60))
-tableLayoutPanel.Controls.Add(buttonLeft, 0, 1)
-let buttonRight = new Button(Text = "RIGHT", Size = new Size(60, 60))
-tableLayoutPanel.Controls.Add(buttonRight, 2, 1)
-let buttonStop = new Button(Text = "STOP", Size = new Size(60, 60))                    
-tableLayoutPanel.Controls.Add(buttonStop, 1, 1)
+let tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel()
+let buttonStop = new System.Windows.Forms.Button()
+let buttonLeft = new System.Windows.Forms.Button()
+let buttonRight = new System.Windows.Forms.Button()
+let buttonDown = new System.Windows.Forms.Button()
+let buttonUp = new System.Windows.Forms.Button()
 
 // Create text box to choose robots by id
-let IPTextBox0 = new TextBox(Location = new System.Drawing.Point(60, 10), Size = new Size(40, 10))
-form.Controls.Add(IPTextBox0)
-let IPTextBox1 = new TextBox(Location = new System.Drawing.Point(100, 10), Size = new Size(40, 10))
-form.Controls.Add(IPTextBox1)
-let IPTextBox2 = new TextBox(Location = new System.Drawing.Point(140, 10), Size = new Size(40, 10))
-form.Controls.Add(IPTextBox2)
-let IPTextBox3 = new TextBox(Location = new System.Drawing.Point(180, 10), Size = new Size(40, 10))
-form.Controls.Add(IPTextBox3)
-let IPlabel = new Label(Text = "IP:", Top = 10, Left = 40, AutoSize = true)
-form.Controls.Add(IPlabel)
+let createTextBox() =
+    new TextBox(Size = new Size(40, 10))
+let IPTextBox0 = createTextBox()
+let IPTextBox1 = createTextBox()
+let IPTextBox2 = createTextBox()
+let IPTextBox3 = createTextBox()
+let IPlabel = new Label(Text = "IP:", AutoSize = true)
+let minTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel()
+minTableLayoutPanel.ColumnCount <- 5
+minTableLayoutPanel.RowCount <- 1
+minTableLayoutPanel.SetColumnSpan(minTableLayoutPanel, 3)
+minTableLayoutPanel.Controls.Add(IPlabel, 0, 0)
+minTableLayoutPanel.Controls.Add(IPTextBox0, 1, 0)
+minTableLayoutPanel.Controls.Add(IPTextBox1, 2, 0)
+minTableLayoutPanel.Controls.Add(IPTextBox2, 3, 0)
+minTableLayoutPanel.Controls.Add(IPTextBox3, 4, 0)
+
+tableLayoutPanel1.Dock <- DockStyle.Fill
+tableLayoutPanel1.ColumnCount <- 3
+for i = 0 to 2 do
+    tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F)) |> ignore
+tableLayoutPanel1.RowCount <- 4
+tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30.0F)) |> ignore
+for i = 0 to 2 do
+    tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F)) |> ignore
+tableLayoutPanel1.Controls.Add(buttonStop, 1, 2)
+tableLayoutPanel1.Controls.Add(buttonLeft, 0, 2)
+tableLayoutPanel1.Controls.Add(buttonUp, 1, 1)
+tableLayoutPanel1.Controls.Add(buttonRight, 2, 2)
+tableLayoutPanel1.Controls.Add(buttonDown, 1, 3)
+tableLayoutPanel1.Controls.Add(minTableLayoutPanel, 0, 0)
+tableLayoutPanel1.MinimumSize <- new System.Drawing.Size(220, 120)
+
+let createButton (button : System.Windows.Forms.Button) =
+    button.Dock <- DockStyle.Fill
+    button.MinimumSize <- new System.Drawing.Size(70, 30)
+    button.Text <- "Stop"
+
+createButton buttonStop
+createButton buttonLeft
+createButton buttonRight
+createButton buttonDown 
+createButton buttonUp
+
+form.Controls.Add(tableLayoutPanel1)
 
 // auto write for testing
 let autoWrite() = 
--- 4semester/Coursework/server/Program.fs
+++ 4semester/Coursework/server/Program.fs
@@ -9,15 +9,13 @@ let listener =
     let tcpListener = new TcpListener(serverIP, 1234)
     tcpListener.Start()
     while true do
-         if tcpListener.Pending() 
-         then
-            let timer = new System.Activities.Statements.Delay()
-            printf("I have a request \n")
-            let tcpClient = tcpListener.AcceptTcpClient() 
-            let myStream = tcpClient.GetStream()
-            let streamForJS = new System.IO.StreamReader(myStream)
-            let button = streamForJS.ReadLine()
-            System.Console.WriteLine(button)
-            tcpListener.Stop()
-            tcpListener.Start()
+        if tcpListener.Pending() 
+            then
+                let tcpClient = tcpListener.AcceptTcpClient() 
+                let myStream = tcpClient.GetStream()
+                let streamForJS = new System.IO.StreamReader(myStream)
+                let button = streamForJS.ReadLine()
+                System.Console.WriteLine(button)
+                tcpListener.Stop()
+                tcpListener.Start()
     
